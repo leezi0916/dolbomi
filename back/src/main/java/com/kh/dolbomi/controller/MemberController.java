c6f4738c54da5eb3efdc9699db5cc7dfbcd62ef4
KoesJin <wlstjr2011@naver.com>
2025-06-11 12:49:41 +0900

    chore : 프론트 , 백 동시 테스트

--- back/src/main/java/com/kh/dolbomi/controller/MemberController.java
+++ back/src/main/java/com/kh/dolbomi/controller/MemberController.java
@@ -12,5 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin // 프론트와 백엔드 url이 다른것을 맞춰주기 위한 어노테이션
 public class MemberController {
 
-
+    public void aaa() {
+        
+    }
 }
