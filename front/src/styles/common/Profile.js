fd5bf0210f8140353086763fbff07916835e875c
KoesJin <wlstjr2011@naver.com>
2025-06-12 11:34:27 +0900

    feat : 공통 컴포넌트 예시 파일 작성

---
+++ front/src/styles/common/Profile.js
@@ -0,0 +1,7 @@
+import { styled } from 'styled-components';
+
+// 예시 코드입니다 지우고 새롭게 만들어주세요
+// 단 기본적으로 맨위의 틀을가지고 가서 변경하는것을 추천하나, 많이 바뀔거같으면 아래와 같이 새로 하나 만들어서 사용하세요
+// 절대 다른 사람이 만든 컴포넌트 말없이 수정 ㄴㄴ 당신 책임
+
+export const profile = styled.img``;
