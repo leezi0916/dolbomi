a3dddf37e0787e2f314ebde78c9a37aa0e124548
leezi0916 <leezi0916@naver.com>
2025-06-19 15:35:47 +0900

    merge: 헤더병합 수정

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -123,32 +123,37 @@ const Header = () => {
                     돌봄대상자 관리
                   </NavItem>
                 ) : (
-                  <NavItem to="/caregiver/resumemanagement">
+                  <NavItem to="/">
                     <Icon src="/src/assets/icons/icon_이력서등록.png" alt="" />
                     이력서 등록
                   </NavItem>
                 )}
 
                 {userStatus ? (
-                  <NavItem to="/hireDetail/hiringNo">
+                  <NavItem to="/guardian/hire-registration">
                     <Icon src="/src/assets/icons/icon_이력서등록.png" alt="" />
                     돌봄대상자 신청
                   </NavItem>
                 ) : (
-                  <NavItem to="/">
-                    <Icon src="/src/assets/icons/icon_간병사신청.png" alt="" />
-                    간병사 신청
-                  </NavItem>
+                  ''
                 )}
 
-                <NavItem to="/">
+                <NavItem to="/history-management">
                   <Icon src="/src/assets/icons/icon_내역관리.png" alt="" />
                   내역관리
                 </NavItem>
-                <NavItem to="/">
-                  <Icon src="/src/assets/icons/icon_리뷰페이지.png" alt="" />
-                  리뷰페이지
-                </NavItem>
+
+                {userStatus ? (
+                  <NavItem to="/">
+                    <Icon src="/src/assets/icons/icon_리뷰페이지.png" alt="" />
+                    내가쓴리뷰
+                  </NavItem>
+                ) : (
+                  <NavItem to="/">
+                    <Icon src="/src/assets/icons/icon_리뷰페이지.png" alt="" />
+                    받은리뷰
+                  </NavItem>
+                )}
 
                 {userStatus ? (
                   <NavItem to="/guardian/matchpage">
