2cb886ec323349b8ae088d0f3ab030b383e5e768
KoesJin <wlstjr2011@naver.com>
2025-06-19 15:35:08 +0900

    chore : Paging바 Component 분리

--- front/src/components/Paging.jsx
+++ front/src/components/Paging.jsx
@@ -3,61 +3,42 @@ import { useState } from 'react';
 import styled from 'styled-components';
 import { Pagination, Stack } from '@mui/material';
 
-const Paging = () => {
-    // 데이터의 갯수
-    const data = ["",""];
-  // 몇개씩 보여줄지
-  const ITEMS_PER_PAGE = 10;
-
-  const [currentPage, setCurrentPage] = useState(1); 
-  
-  const offset = (currentPage - 1) * ITEMS_PER_PAGE; 
-  
-  // 전체페이지
-  const totalPage = Math.ceil(data.length / ITEMS_PER_PAGE);
-
-  console.log(currentPage,"/",totalPage,"");
-
-  return(
- 
- <>
- 
-    <PaginationWrapper>
-      <Stack spacing={2}>
-        <Pagination
-          count={totalPage}
-          page={currentPage}
-          onChange={(e, value) => setCurrentPage(value)}
-          shape="rounded"
-          showFirstButton
-          showLastButton
-          sx={{
-            '& .MuiPaginationItem-root': {
-              color: '#FFA101',
-              borderColor: '#FFA101',
-            },
-            '& .Mui-selected': {
-              backgroundColor: '#FFA101 !important',
-              color: '#fff',
-            },
-            '& .MuiPaginationItem-icon': {
-              color: '#FFA101',
-            },
-          }}
-        />
-      </Stack>
-    </PaginationWrapper>
-    
-  </>
+const Paging = ({ totalPage, currentPage, chagneCurrentPage }) => {
+  return (
+    <>
+      <PaginationWrapper>
+        <Stack spacing={2}>
+          <Pagination
+            count={totalPage}
+            page={currentPage}
+            onChange={(e, value) => chagneCurrentPage(value)}
+            shape="rounded"
+            showFirstButton
+            showLastButton
+            sx={{
+              '& .MuiPaginationItem-root': {
+                color: '#FFA101',
+                borderColor: '#FFA101',
+              },
+              '& .Mui-selected': {
+                backgroundColor: '#FFA101 !important',
+                color: '#fff',
+              },
+              '& .MuiPaginationItem-icon': {
+                color: '#FFA101',
+              },
+            }}
+          />
+        </Stack>
+      </PaginationWrapper>
+    </>
   );
-  
 };
 
 export default Paging;
-
 
 const PaginationWrapper = styled.div`
   display: flex;
   justify-content: center;
   margin-top: ${({ theme }) => theme.spacing[5]};
-  `
+`;
