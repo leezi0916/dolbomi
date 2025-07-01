3d58b7e29053d064389c9c9dec995b3c1fa336f7
LEE\ashes <ashes999@naver.com>
2025-07-01 10:28:03 +0900

    feat : 매칭 정보 가져오기(get)

--- back/src/main/java/com/kh/dolbomi/repository/MatchingRepositoryImpl.java
+++ back/src/main/java/com/kh/dolbomi/repository/MatchingRepositoryImpl.java
@@ -13,22 +13,12 @@ public class MatchingRepositoryImpl implements MatchingRepository {
     private EntityManager em;
 
     public List<Object[]> findbyPatNo(Long patNo, StatusEnum.Status matchingStatus) {
-//        String query = """
-//                SELECT m , u.age , u.gender
-//                FROM Matching m
-//                JOIN m.caregiver u
-//                WHERE m.patient.patNo = :patNo
-//                AND m.status = :matchingStatus""";
-//
-//        List<Matching> resultList = em.createQuery(query, Matching.class)
-//                .setParameter("patNo", patNo)
-//                .setParameter("matchingStatus", matchingStatus)
-//                .getResultList();
-
-        String query =
-                "SELECT r.matNo, u.userName, u.age, r.startDate "
-                        + "FROM Matching r JOIN r.caregiver u " +
-                        "WHERE r.status = 'Y' AND p.patNo = :patNo ";
+        String query = """
+                SELECT m.matNo , u.userNo, u.userName , u.age , u.gender, m.startDate, m.status
+                FROM Matching m
+                JOIN m.caregiver u
+                WHERE m.patient.patNo = :patNo
+                AND m.status = :matchingStatus""";
 
         return em.createQuery(query, Object[].class)
                 .setParameter("patNo", patNo)
