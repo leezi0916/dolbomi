59a5818f8f5cbad48cd68ac6fc8b2bfe1e3b008e
leezi0916 <leezi0916@naver.com>
2025-07-02 21:54:15 +0900

    chore: 병합 수정

--- back/src/main/java/com/kh/dolbomi/service/MatchingService.java
+++ back/src/main/java/com/kh/dolbomi/service/MatchingService.java
@@ -14,8 +14,7 @@ public interface MatchingService {
     Page<MatchingDto.Response> getMatchedListByStatus(Long patNo, Status status, Pageable pageable);
 
     List<MatchingDto.ResponsePat> getMatchingListCaregiver(Long caregiverNo, Status matchingStatus);
-
-
+    
     Long changeStatus(Long matNo, Status matchingStatus);
 
     // 종료된 매칭 목록 페이징 조회 - 간병인 version
