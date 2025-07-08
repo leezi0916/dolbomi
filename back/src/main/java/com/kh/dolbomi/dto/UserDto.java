c9e0d1a6a5124f43c3b555e2a2c34315be51ddf8
leezi0916 <leezi0916@naver.com>
2025-07-08 11:09:23 +0900

    feat: s3 마이페이지 업로드

--- back/src/main/java/com/kh/dolbomi/dto/UserDto.java
+++ back/src/main/java/com/kh/dolbomi/dto/UserDto.java
@@ -136,6 +136,7 @@ public class UserDto {
         private Integer age;
         private StatusEnum.Gender gender;
         private List<LicenseDto.Response> licenses;
+        private String profile_image;
 
         public static ProfileDto toDto(User user) {
             return ProfileDto.builder()
@@ -152,9 +153,10 @@ public class UserDto {
                                     .map(LicenseDto::toDto)
                                     .collect(Collectors.toList())
                     )
+                    .profile_image(user.getProfileImage())
                     .build();
         }
     }
 
-    
+
 }
