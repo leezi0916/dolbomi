9d3f55aacab3f2d47648e41aa538055bdd48792d
UHa11 <extramin93@gmail.com>
2025-07-11 12:47:26 +0900

    fix: - 매칭된 간병보기 -> 매칭된 돌봄대상자 보기 로 바꿈. - 매칭 종료 글쓰기 안보이게

--- front/src/pages/MatchToPatient.jsx
+++ front/src/pages/MatchToPatient.jsx
@@ -21,7 +21,6 @@ import {
 } from '../styles/MatchingCard';
 import { media } from '../styles/MediaQueries';
 
-
 const MatchToPatient = () => {
   const [activeTab, setActiveTab] = useState('matching');
   const { user } = useUserStore();
@@ -125,7 +124,7 @@ const MatchToPatient = () => {
     <>
       <HeadSection>
         <TitleDiv>
-          <Title>매칭된 간병보기</Title>
+          <Title>매칭된 돌봄대상자 보기</Title>
           <Tab>
             <SubTitle onClick={() => handleTabChange('matching')} $active={activeTab === 'matching'}>
               진행중
@@ -155,8 +154,7 @@ const MatchToPatient = () => {
 
       {/*진행중 매칭 */}
       <MatchSection>
-        <CardWrap list={activeTab === 'matching' ? patientList : endedPatientList}
-  > 
+        <CardWrap list={activeTab === 'matching' ? patientList : endedPatientList}>
           {activeTab === 'matching' && (
             <>
               {patientList && patientList.length > 0 ? (
@@ -175,9 +173,7 @@ const MatchToPatient = () => {
                         </TextWrap>
                         <TextWrap>
                           <img src="/src/assets/icons/icon_gender.png" />
-                          <p>
-                          {pat.patGender === 'F' ? '여성' : '남성'}
-                          </p>
+                          <p>{pat.patGender === 'F' ? '여성' : '남성'}</p>
                         </TextWrap>
                       </InfoSection>
                     </ProfileSection>
@@ -216,9 +212,7 @@ const MatchToPatient = () => {
                         </TextWrap>
                         <TextWrap>
                           <img src="/src/assets/icons/icon_gender.png" />
-                          <p>
-                            {pat.patGender === 'F' ? '여성' : '남성'}
-                          </p>
+                          <p>{pat.patGender === 'F' ? '여성' : '남성'}</p>
                         </TextWrap>
                         <TextWrap>
                           <img src="/src/assets/icons/icon_달력.png" />
@@ -229,7 +223,9 @@ const MatchToPatient = () => {
                       </InfoSection>
                     </ProfileSection>
                     <BtnSection>
-                      <InfoButton onClick={() => navigate(`/report/${pat.patNo}`)}>간병일지 보기</InfoButton>
+                      <InfoButton onClick={() => navigate(`/report/${pat.patNo}`, { state: { status: 'N' } })}>
+                        간병일지 보기
+                      </InfoButton>
                     </BtnSection>
                   </CardInnerWrap>
                 ))
@@ -246,15 +242,13 @@ const MatchToPatient = () => {
 };
 
 const HeadSection = styled(Section)`
-
   display: flex;
   height: auto;
   justify-content: space-between;
-  padding: ${({ theme }) => theme.spacing[4]} ${({ theme }) => theme.spacing[4]} ${({ theme }) => theme.spacing[2]} ${({ theme }) => theme.spacing[4]}; /* 모바일 기본 패딩 */
+  padding: ${({ theme }) => theme.spacing[4]} ${({ theme }) => theme.spacing[4]} ${({ theme }) => theme.spacing[2]}; /* 모바일 기본 패딩 */
   align-items: flex-start; /* 모바일 기본: 왼쪽 정렬 */
   ${media.md` /* 768px 이상 (태블릿/데스크톱) */
     padding: 40px 16px 10px 16px;
-   
   `}
 `;
 
